71a9abd302ac30d4a71060d591eebc0258b1c65d
Raj Rathore <rajrathore15755@gmail.com>
2025-01-12 17:56:58 +0530

    newChanges

---
+++ basicarray.js
@@ -0,0 +1,23 @@
+
+//when you create  an arr by using an array so you can use map
+// let ar = [1,2,3,4];
+
+// var a = ar.map(function(key){
+// return key+1;
+// })
+// console.log(a);
+
+// filter just filter out the array and print filtered out array 
+// let ar2 = [1,2,3,4];
+
+// var a2 = ar2.filter(function(key){
+// if(key>3){
+//     return true;}
+// else
+// return false; 
+// })
+// console.log(a2);
+
+//find and indexOf method is use to find an element and index 
+
+
